f2f0025bdc177b145118f5556969de8a2f464a2e
Prince raj <princeraj123letsrun@gmail.com>
2025-10-20 00:57:25 +0530

    google and github auth done

---
+++ app/ui/hello-world/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+import React from 'react'
+import { signout } from '@/lib/auth-actions'
+
+const page = () => {
+  return (
+    <div>
+      <p>Hello world</p>
+      <button onClick={()=>signout()}>sign up</button>
+    </div>
+  )
+}
+
+export default page
